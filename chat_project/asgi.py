2932bc1d86be2dc76c6cf80065efdd94a8a266ac
Jaimitha13 <jaimithalk@gmail.com>
2025-08-07 23:27:01 +0530

    Initial commit - Django chat app with Redis

---
+++ chat_project/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for chat_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+import django
+from channels.routing import get_default_application
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+django.setup()
+application = get_asgi_application()
+
